5e76df9090ab623c1d1df798ccd29ee561cf40ba
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-02-27 17:40:42 +0100

    feat: fine esercizio

--- js/script.js
+++ js/script.js
@@ -45,74 +45,75 @@ mySubmitBtnEl.addEventListener('click', function(){
 let userWin = [];
 let computerWin = [];
 
+
 const startBtnEl = document.getElementById('start-game');
 const winnerBannerEl = document.getElementById('winner-banner');
 
-
-
+const partialUserEl = document.getElementById('partial-user');
+const partialComputerEl = document.getElementById('partial-computer');
+const partialScoreEl = document.querySelector('.__partial-score');
+console.log(partialScoreEl);
 
 
 startBtnEl.addEventListener('click', function(){
-    while (userWin.length < 5 && computerWin.length < 5){
-      let userGuessInputEl = document.getElementById('userGuessInput');
-      let userGuess = Number(userGuessInputEl.value);
-      if (isNaN(userGuess) || userGuess < 1 || userGuess > 6) {
-        window.alert(('Devi inserire un numero tra 1 e 6'));
-        break;
-     
+  partialScoreEl.style.display = 'flex';
+
+
+  while (userWin.length < 5 && computerWin.length < 5){  
+
+    
+    let userGuessInputEl = document.getElementById('userGuessInput');
+    let userGuess = Number(userGuessInputEl.value);
+    if (isNaN(userGuess) || userGuess < 1 || userGuess > 6) {
+      window.alert(('Devi inserire un numero tra 1 e 6'));
+      break;    
+    }else{       
+      console.log(userWin, computerWin);
+      
+      // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+      let randomNumber = Math.floor(Math.random() * 6) + 1;
+      
+      let computerGuess = Math.floor(Math.random() * 6) + 1;
+      console.log(randomNumber, computerGuess);
+        
+      // controllo che l'input dell'utente sia tra 1 e 6
+      
+      // vince il primo che arriva a 5 risposte esatte
+      if(userGuess === randomNumber){
+        userWin.push('1')
+        partialUserEl.innerHTML = `${userWin.length}`;
+        
+        alert(('Hai indovinato!'));
+        // partialUserEl.innerText = `Il tuo punteggio provvisorio è: ${userWin.length}`;
+      
+      }else if (computerGuess === randomNumber){
+        computerWin.push('1');
+        partialComputerEl.innerHTML = `${computerWin.length}`;
+
+        alert(('Il computer ha indovinato!'));
+        // partialComputerEl.innerText = `Il punteggio provvisorio del computer è: ${computerWin.length}`
+                  
       }else{
         
-        console.log(userGuess);
-        console.log(userWin, computerWin);
-        
-        // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-        let randomNumber = Math.floor(Math.random() * 6) + 1;
-        
-        let computerGuess = Math.floor(Math.random() * 6) + 1;
-        console.log(randomNumber, computerGuess);
-          
-      
-        // controllo che l'input dell'utente sia tra 1 e 6
-        
-      
-        // vince il primo che arriva a 5 risposte esatte
-        if(userGuess === randomNumber){
-          userWin.push('1')
-          
-          alert(('Hai indovinato!'));
-          
-      
-        }else if (computerGuess === randomNumber){
-          computerWin.push('1')
-          
-          alert(('Il computer ha indovinato!'));
-      
-          
-        }else{
-          
-          alert(('Mi dispiace, nessuno ha indovinato!'));
-      
-        }
-        
-        
-        // controlla se quqalcuno ha vinto
-        if (userWin.length === 5) {
-          winnerBannerEl.style.display = 'block'
-          winnerBannerEl.innerHTML = 'Hai Vinto'
-          break;
-        } else if (computerWin.length === 5) {
-          winnerBannerEl.style.display = 'block'
+        alert(('Mi dispiace, nessuno ha indovinato!'));
+      }
+              
+      // controlla se quqalcuno ha vinto
+      if (userWin.length === 5) {
+        winnerBannerEl.style.display = 'block';
+        winnerBannerEl.innerHTML = `Hai vinto ${userWin.length} a ${computerWin.length}`;
+        break;
+      } else if (computerWin.length === 5) {
+        winnerBannerEl.style.display = 'block';
 
-          winnerBannerEl.innerHTML = 'Il computer ha vinto'
-          break;
-        }
-        // Reset the input field to allow the user to input another guess
-        userGuessInputEl.value = '';
-      } 
-      
-    }
+        winnerBannerEl.innerHTML = `Il computer ha vinto ${computerWin.length} a ${userWin.length}`;
+        break;
+      }
+      // Reset the input field to allow the user to input another guess
+      userGuessInputEl.value = '';
+    } 
+    
+  }
         
     
 });
-
-
