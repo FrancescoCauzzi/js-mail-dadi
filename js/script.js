2c9d5c34b9c31333f0289113fd6a97a19a00e61f
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-02-27 13:08:47 +0100

    feat: implementazione codice 'indovina il numero'

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-
+/*
+// esercizo 1 email
 const emails = ["lknope@pawneeparks.gov", "eliza.doolittle@otmail.com", "pinocchio@realboys.com", "hodor@hodor.hodor","giovannimucciaccia@gmail.com"];
 
 
-if (true){
+while (true){
+  // Chiedi all’utente la sua email,
+
   let userEmail = prompt('Inserisci la tua email');
   const allowed = [];
-  const notAllowed = [];
   for( let i = 0; i < emails.length; i++){
+  // controlla che sia nella lista di chi può accedere,
   
     if(userEmail === emails[i]){
       allowed.push(userEmail);
@@ -14,15 +17,61 @@ if (true){
       continue;
     }    
   }
+  // stampa un messaggio appropriato sull’esito del controllo.
   if (allowed.length > 0){
     console.log('Accesso consentito');
+    break;
     
   }else{
     console.log('Accesso negato');
-    
+    break;
   }
 
 }
 
+*/
+// esercizio 2 dadi
 
+const userWin = [];
+const computerWin = [];
 
+while (userWin.length < 5 && computerWin.length < 5){
+  console.log(userWin, computerWin);
+  // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  
+  let computerGuess = Math.floor(Math.random() * 6) + 1;
+  console.log(randomNumber, computerGuess);
+  // l'utente genera la sua risposta
+  let userGuess = parseInt(prompt('Scegli un numero da 1 a 6'));  
+
+  // controllo che l'input dell'utente sia tra 1 e 6
+  if (isNaN(userGuess) || userGuess < 1 || userGuess > 6) {
+    alert('Devi inserire un numero tra 1 e 6');
+    continue;
+  }
+
+  // vince il primo che arriva a 5 risposte esatte
+  if(userGuess === randomNumber){
+    userWin.push('1')
+    console.log(userWin);
+    alert('Hai indovinato!');
+
+  }else if (computerGuess === randomNumber){
+    computerWin.push('1')
+    console.log(computerWin);
+    alert('Il computer ha indovinato!');
+  }else{
+    alert('Mi dispiace, nessuno ha indovinato!');
+  }
+  
+  // controlla se quqalcuno ha vinto
+  if (userWin.length === 5) {
+    alert('Hai vinto!');
+    break;
+  } else if (computerWin.length === 5) {
+    alert('Il computer ha vinto!');
+    break;
+  }
+
+}
